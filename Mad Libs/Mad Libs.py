ce51ba2411b2e7afdeb9d8841634d36c2dbce747
Gautam Nag <51375618+gautamnag279@users.noreply.github.com>
2021-03-07 11:35:07 +0530

    Create Mad Libs.py

---
+++ Mad Libs/Mad Libs.py
@@ -0,0 +1,21 @@
+loop = 1
+
+while (loop < 10):
+    relationship = input("Choose a relationaship (eg. father, sister..etc): " , ).upper()
+    adj = input("Choose an adjective (eg. beautiful, chubby..etc): " , ).upper()
+    place = input("Choose a place (eg. the market, my home...etc): " , ).upper()
+    number = int(input("Choose a number: " , ))
+    action = input("Choose an action (eg. cooking, running..etc): " , ).upper()
+    dirtyWord = input("Choose a dirty word ending in 'ing': " , ).upper()
+
+    print("------------------------------------------------------------------------------------------------------------------")
+    print("I love" , action)
+    print("But what I love more than" , action , "is" , dirtyWord)
+    print("See the thing is" , action , "can be done anywhere. But," , dirtyWord , "can only be enjoyed in" , place)
+    print("And this is what makes" , place , "a perfect destination!")
+    print("The last time I recall" , action , "my" , relationship , "was" , number , "light years ago")
+    print("But our" , dirtyWord , "days are as old as yesterday")
+    print("No wonder my joints are sore!")
+    print("------------------------------------------------------------------------------------------------------------------")
+
+    loop += 1
